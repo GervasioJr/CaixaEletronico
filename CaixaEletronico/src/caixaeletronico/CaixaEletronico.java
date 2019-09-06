231682ad7701e39e0d781bd2ccb5342db824b6a3
gabriellenhart <augustolenhart@gmail.com>
2019-09-05 21:52:56 -0300

    Já consigo sacar notas iguais;

--- CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
+++ CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
@@ -1,12 +1,22 @@
-
 package caixaeletronico;
-
 
 public class CaixaEletronico {
 
-
     public static void main(String[] args) {
-        // TODO code application logic here
+        int valor = 10;
+        int[] notas = new int[]{2, 5, 10, 20, 50, 100};
+
+        int qtNotas = notas.length - 1; // Quantidade de notas: 6
+        for (int i = qtNotas; i >= 0; i--) {
+            if (valor % notas[i] == 0) {
+                System.out.println(valor / notas[i] +
+                        " x " + notas[i]);
+            }
+//            if (notas[i] <= valor) {
+//                notasUsadas[i] = notas[i];
+//            }
+        }
+
     }
-    
+
 }
