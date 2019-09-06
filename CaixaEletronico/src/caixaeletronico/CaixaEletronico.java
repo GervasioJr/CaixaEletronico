21e1722aaa384c9ff0ff815993a18981c60f5366
gabriellenhart <augustolenhart@gmail.com>
2019-09-05 22:27:07 -0300

    Possibilidades

--- CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
+++ CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
@@ -3,18 +3,28 @@ package caixaeletronico;
 public class CaixaEletronico {
 
     public static void main(String[] args) {
-        int valor = 10;
+        int valor = 12;
         int[] notas = new int[]{2, 5, 10, 20, 50, 100};
 
         int qtNotas = notas.length - 1; // Quantidade de notas: 6
         for (int i = qtNotas; i >= 0; i--) {
-            if (valor % notas[i] == 0) {
-                System.out.println(valor / notas[i] +
-                        " x " + notas[i]);
+            if (valor >= notas[i]) {
+                int notaAtual = valor / notas[i];
+                int resto = valor % notas[i];
+                System.out.println(valor / notas[i]
+                        + " x " + notas[i]);
+                if (resto > 0) {
+                    for (int j = i - 1; j >= 0; j--) {
+                        if (notas[j] <= resto) {
+                            notaAtual = resto / notas[j];
+                            System.out.println(notaAtual + " x " + notas[j]);
+                            
+                        }
+
+                    }
+                }
+                System.out.println("--------------");
             }
-//            if (notas[i] <= valor) {
-//                notasUsadas[i] = notas[i];
-//            }
         }
 
     }
